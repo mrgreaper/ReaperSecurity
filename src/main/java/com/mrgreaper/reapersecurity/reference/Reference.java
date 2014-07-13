e0afcbc1be3ad1f5964c8fa1d1e552e840060120
David Eastwick <mrgreaper2004@gmail.com>
2014-07-14 00:23:04 +0100

    fixed hicups with proxy

--- src/main/java/com/mrgreaper/reapersecurity/reference/Reference.java
+++ src/main/java/com/mrgreaper/reapersecurity/reference/Reference.java
@@ -7,8 +7,8 @@ public class Reference {
     public static final String MODID = "reapersecurity";
     public static final String VERSION = "1.7.10.1.0";
     public static final String MOD_NAME = "Reaper Security";
-    public static final String CLIENT_PROXY_CLASS = "com.mrgreaper.reapercore.proxy.ClientProxy";
-    public static final String SERVER_PROXY_CLASS = "com.mrgreaper.reapercore.proxy.ServerProxy";
-    public static final String GUI_FACTORY_CLASS = "com.mrgreaper.reaperore.client.gui.GuiFactory";
+    public static final String CLIENT_PROXY_CLASS = "com.mrgreaper.reapersecurity.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.mrgreaper.reapersecurity.proxy.ServerProxy";
+    public static final String GUI_FACTORY_CLASS = "com.mrgreaper.reapersecurity.client.gui.GuiFactory";
 
 }
